e33013e8f59b238262270787672f7235fd665ffc
mdyeamin <ahmedyeamin05@gmail.com>
2026-02-23 23:27:36 +0600

    show total job from length

---
+++ function/global.js
@@ -0,0 +1,7 @@
+// hwo many jon available
+// get the all job by class
+const jobs = document.getElementsByClassName("job-card");
+// where i show job length
+const totalJob = document.getElementById("total");
+totalJob.innerText = jobs.length
+
