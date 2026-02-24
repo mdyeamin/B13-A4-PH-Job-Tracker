1fbfaeb9add45221c555e1a1822e13cc66a3672c
mdyeamin <ahmedyeamin05@gmail.com>
2026-02-24 12:37:27 +0600

    delete button updated

--- function/global.js
+++ function/global.js
@@ -1,17 +1,18 @@
-
 const jobs = document.getElementsByClassName("job-card");
 const totalJob = document.getElementById("total-jobs");
 const allJobs = document.getElementById("all-jobs");
-totalJob.innerText = jobs.length
-allJobs.innerText = jobs.length
-
-
-// delete card 
-document.getElementById("all-job-preview").addEventListener("click",function(e){
-    (e.target.classList.contains("delete-card"))
-        e.target.closest(".job-card").remove();
-
-    
-    console.log(222);
-    
-})
+function updateJobCount() {
+  totalJob.innerText = jobs.length;
+  allJobs.innerText = jobs.length;
+}
+updateJobCount();
+// delete card
+document
+  .getElementById("all-job-preview")
+  .addEventListener("click", function (e) {
+    const deleteBtn = e.target.closest(".delete-card");
+    if (deleteBtn) {
+      deleteBtn.closest(".job-card").remove();
+      updateJobCount();
+    }
+  });
