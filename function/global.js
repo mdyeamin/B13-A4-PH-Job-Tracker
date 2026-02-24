fddb3128cd50ff3878a2ee637074eb185115565d
mdyeamin <ahmedyeamin05@gmail.com>
2026-02-24 12:26:58 +0600

    delete button working

--- function/global.js
+++ function/global.js
@@ -6,3 +6,12 @@ totalJob.innerText = jobs.length
 allJobs.innerText = jobs.length
 
 
+// delete card 
+document.getElementById("all-job-preview").addEventListener("click",function(e){
+    (e.target.classList.contains("delete-card"))
+        e.target.closest(".job-card").remove();
+
+    
+    console.log(222);
+    
+})
