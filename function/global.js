99d6319a81359bb8d65b2e4da9efdf1c4de38bd9
mdyeamin <ahmedyeamin05@gmail.com>
2026-02-24 14:29:03 +0600

    interview and rejected button toggle was an issue , it done solve

--- function/global.js
+++ function/global.js
@@ -1,10 +1,16 @@
 const jobs = document.getElementsByClassName("job-card");
 const totalJob = document.getElementById("total-jobs");
 const allJobs = document.getElementById("all-jobs");
+
+const interviewList = document.getElementById("interview-preview");
+// const  totalinterview =
 function updateJobCount() {
   totalJob.innerText = jobs.length;
   allJobs.innerText = jobs.length;
 }
+
+function interviewCount() {}
+
 updateJobCount();
 // delete card
 document
@@ -17,5 +23,74 @@ document
     }
   });
 
+// when interview and rejected button clicked
+// when interview button click
+document
+  .getElementById("all-job-preview")
+  .addEventListener("click", function (e) {
+    const interviewBtn = e.target.closest(".interview-button");
+    const rejectBtn = e.target.closest(".reject-button");
 
-  
+    if (interviewBtn) {
+      const card = interviewBtn.closest(".job-card");
+      const statusBtn = card.querySelector(".status-button");
+
+      statusBtn.innerText = "INTERVIEW";
+
+      statusBtn.classList.remove("bg-primary/10");
+      statusBtn.classList.add("bg-accent", "text-white");
+
+      const jobTitle = card.querySelector("h5")?.innerText || "";
+      const interviewContainer = document.getElementById("interview-preview");
+
+      const alreadyAdded = Array.from(
+        interviewContainer.querySelectorAll("h5"),
+      ).some((t) => t.innerText === jobTitle);
+
+      if (!alreadyAdded) {
+        const clonedCard = card.cloneNode(true);
+
+        clonedCard.classList.remove("job-card");
+        clonedCard.classList.add("interview-card");
+
+        const statusBtn = clonedCard.querySelector(".status-button");
+        if (statusBtn) {
+          statusBtn.innerText = "INTERVIEW";
+          statusBtn.classList.remove("bg-primary/10", "bg-secondary");
+          statusBtn.classList.add("bg-accent", "text-white");
+        }
+
+        interviewContainer.appendChild(clonedCard);
+      }
+    }
+
+    if (rejectBtn) {
+      const card = rejectBtn.closest(".job-card");
+      const statusBtn = card.querySelector(".status-button");
+      statusBtn.innerText = "REJECTED";
+      statusBtn.classList.add("bg-secondary", "text-white");
+      statusBtn.classList.remove("bg-accent", "bg-primary/10");
+      const jobTitle = card.querySelector("h5")?.innerText || "";
+      const rejectedContainer = document.getElementById("rejected-preview");
+
+      const alreadyAdded = Array.from(
+        rejectedContainer.querySelectorAll("h5"),
+      ).some((t) => t.innerText === jobTitle);
+
+      if (!alreadyAdded) {
+        const clonedCard = card.cloneNode(true);
+
+        clonedCard.classList.remove("job-card");
+        clonedCard.classList.add("rejected-card");
+
+        const statusBtn = clonedCard.querySelector(".status-button");
+        if (statusBtn) {
+          statusBtn.innerText = "REJECTED";
+          statusBtn.classList.remove("bg-primary/10", "bg-accent");
+          statusBtn.classList.add("bg-secondary", "text-white");
+        }
+
+        rejectedContainer.appendChild(clonedCard);
+      }
+    }
+  });
